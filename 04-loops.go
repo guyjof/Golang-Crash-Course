d7bc849972bcaef4f66574b0d59954475d1dafdb
guyjof <guyjofre02@gmail.com>
2024-09-15 21:17:59 +0300

    chore: Add examples of for loops in 04-loops.go

---
+++ 04-loops.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for loop
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i)
+	}
+
+	// this is the same as the for loop above
+	// j := 1
+	// for j <= 5 {
+	// 	fmt.Println(j)
+	// 	j++
+	// }
+	// this is the same as the for loop above
+	// k := 1
+
+	// for {
+	// 	if k > 5 {
+	// 		break
+	// 	}
+	// 	fmt.Println(k)
+	// 	k++
+	// }
+}
